abde31b42d998adb3861a8aee10aa0c8fa008561
phanvanhuy <vanhuy812.dev@gmail.com>
2026-08-28 21:55:22 +0700

    update: target api android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
     defaultConfig {
         applicationId = "com.sosconnect.app"
         minSdk = flutter.minSdkVersion
-        targetSdk = 35
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
